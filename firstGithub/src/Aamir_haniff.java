c24690f75724413fb2cba86ba185e494f3e0696c
YouGotBonked <113187422+YouGotBonked@users.noreply.github.com>
2024-02-20 12:21:06 -0500

    Aamir

--- firstGithub/src/Aamir_haniff.java
+++ firstGithub/src/Aamir_haniff.java
@@ -2,7 +2,11 @@ public class Aamir_haniff
 {
     public static void main(String[] args)
     {
-        System.out.println("Hi Aamir!");
+        System.out.println("Hello thine squire Aamir Haniff whomst hath summoned thee");
         System.out.println("Hello good sire Waldron, why hath thy typed the method in the wrong class");
+        for(int i = 0; i<16;i++)
+        {
+            System.out.println("I am "+i+"years old");
+        }
     }
 }
