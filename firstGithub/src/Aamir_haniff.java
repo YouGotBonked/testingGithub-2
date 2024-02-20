97b7b68c4d6529ae34e1d087dc88cf74a484ef87
YouGotBonked <113187422+YouGotBonked@users.noreply.github.com>
2024-02-20 12:17:40 -0500

    Aamir

--- firstGithub/src/Aamir_haniff.java
+++ firstGithub/src/Aamir_haniff.java
@@ -1,2 +1,8 @@
-public class Aamir_haniff {
+public class Aamir_haniff
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hi Aamir!");
+        System.out.println("Hello good sire Waldron, why hath thy typed the method in the wrong class");
+    }
 }
