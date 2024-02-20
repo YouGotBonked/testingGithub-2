5c05c6f43f4f0b76b98a2f23efbae70752489116
YouGotBonked <113187422+YouGotBonked@users.noreply.github.com>
2024-02-20 12:41:28 -0500

    age

--- firstGithub/src/Aamir_haniff.java
+++ firstGithub/src/Aamir_haniff.java
@@ -4,6 +4,9 @@ public class Aamir_haniff
     {
         System.out.println("Hello thine squire Aamir Haniff whomst hath summoned thee");
         System.out.println("Hello good sire Waldron, why hath thy typed the method in the wrong class");
-        
+        for(int i = 0; i<16;i++)
+        {
+            System.out.println("I am "+i+"years old");
+        }
     }
 }
